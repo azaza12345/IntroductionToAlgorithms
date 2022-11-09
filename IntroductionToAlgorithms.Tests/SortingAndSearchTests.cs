5e9ceecd74bcf72ae3113d2ee0617aa450661568
Yaroslav Chesnokov <conyche@partner.waters.com>
2022-11-09 16:30:56 +0300

    Added binary searches

---
+++ IntroductionToAlgorithms.Tests/SortingAndSearchTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SortingAndSearchAlgorithms;
+
+namespace IntroductionToAlgorithms.Tests
+{
+    public class SortingAndSearchTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3}, 3, 2)]
+        [InlineData(new[] { 1, 7, 8}, 1, 0)]
+        [InlineData(new[] { 1, 2, 3}, 5, -1)]
+        [InlineData(new[] { 2, 4, 5, 11}, 5, 2)]
+        [InlineData(new[] { 2, 4, 5, 11}, 4, 1)]
+        [InlineData(new[] { 1, 5, 8, 9, 12}, 12, 4)]
+        public void IterativeBinarySearchTest(int[] array, int target, int result)
+        {
+            // Arrange
+            
+            // Act
+            var value = BinarySearch.IterativeSolution(array, target);
+            
+            // Assert
+            Assert.Equal(result, value);
+        }
+        
+        [Theory]
+        [InlineData(new[] { 1, 2, 3}, 3, 2)]
+        [InlineData(new[] { 1, 7, 8}, 1, 0)]
+        [InlineData(new[] { 1, 2, 3}, 5, -1)]
+        [InlineData(new[] { 2, 4, 5, 11}, 5, 2)]
+        [InlineData(new[] { 2, 4, 5, 11}, 4, 1)]
+        [InlineData(new[] { 1, 5, 8, 9, 12}, 12, 4)]
+        public void RecursiveBinarySearchTest(int[] array, int target, int result)
+        {
+            // Arrange
+            
+            // Act
+            var value = BinarySearch.RecursiveSolution(array, target);
+            
+            // Assert
+            Assert.Equal(result, value);
+        }
+    }
+}
---
+++ SortingAndSearchAlgorithms/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿namespace SortingAndSearchAlgorithms
+{
+    public static class BinarySearch
+    {
+        public static int IterativeSolution(int[] array, int target)
+        {
+            var left = 0;
+            var right = array.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+
+                if (array[middle] == target)
+                    return middle;
+
+                if (array[middle] < target)
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+
+            return -1;
+        }
+
+        public static int RecursiveSolution(int[] array, int target)
+        {
+            var right = array.Length - 1;
+            return Helper(array, target, 0, right);
+        }
+
+        private static int Helper(int[] array, int target, int left, int right)
+        {
+            if (left > right) return -1;
+            var middle = (left + right) / 2;
+
+            if (array[middle] == target)
+                return middle;
+
+            if (array[middle] < target)
+                return Helper(array, target, middle + 1, right);
+            else
+                return Helper(array, target, left, middle - 1);
+        }
+    }
+}
