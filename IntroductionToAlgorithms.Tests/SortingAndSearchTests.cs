7e6c3a73c588c013ee7f85134ec1da1645095184
Yaroslav Chesnokov <conyche@partner.waters.com>
2022-11-10 17:54:13 +0300

    Added selection sort

--- IntroductionToAlgorithms.Tests/SortingAndSearchTests.cs
+++ IntroductionToAlgorithms.Tests/SortingAndSearchTests.cs
@@ -40,5 +40,22 @@ namespace IntroductionToAlgorithms.Tests
             // Assert
             Assert.Equal(result, value);
         }
+        
+        [Theory]
+        [InlineData(new[] { 5, 4}, new[] {4, 5})]
+        [InlineData(new[] { 12, 5, 7}, new[] {5, 7, 12})]
+        [InlineData(new[] { 0, 1, -1}, new[] {-1, 0, 1})]
+        [InlineData(new[] { 2, 1, 4, 3}, new[] {1, 2, 3, 4})]
+        [InlineData(new[] { 9, 8, 7, 6, 5}, new[] {5, 6, 7, 8, 9})]
+        public void SelectionSortTest(int[] array, int[] result)
+        {
+            // Arrange
+            
+            // Act
+            SelectionSort.Solution(array);
+            
+            // Assert
+            Assert.Equal(result, array);
+        }
     }
 }
---
+++ SortingAndSearchAlgorithms/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortingAndSearchAlgorithms
+{
+    public static class SelectionSort
+    {
+        public static void Solution(int[] array)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var smallestIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[smallestIndex])
+                    {
+                        smallestIndex = j;
+                    }
+                }
+
+                (array[i], array[smallestIndex]) = (array[smallestIndex], array[i]);
+            }
+        }
+    }
+}
